f0a09ca7c59c08c1ab3ba1e257556139cae454fe
Roman Kulyk <pomankyluk@gmail.com>
2025-01-12 20:52:35 +0200

    added widgets_4_qcombobox.py file

---
+++ basic/widgets_4_qcombobox.py
@@ -0,0 +1,32 @@
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QComboBox, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My App")
+        widget = QComboBox()
+        widget.addItems(["One", "Two", "Three"])
+        widget.currentIndexChanged.connect(self.index_changed)
+        widget.currentTextChanged.connect(self.text_changed)
+        self.setCentralWidget(widget)
+
+    def index_changed(self, i):  # i is an int
+        print(i)
+
+    def text_changed(self, s):  # s is a str
+        print(s)
+
+
+# Create an instance of QApplication
+app = QApplication(sys.argv)
+
+# Create an instance of a QMainWindow
+window = MainWindow()
+window.show()
+
+# To start up the event loop
+app.exec_()
